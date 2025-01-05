7bd4b07b4e50f61f442ea1c8a79374dd911f6399
Kedharnadh <adurikedharnadh@gmail.com>
2025-01-05 13:05:03 +0530

    Update All.groovy

--- vars/All.groovy
+++ vars/All.groovy
@@ -1,24 +1,24 @@
-def call() {
+def buildproject() {
     echo 'Building project with Maven...'
     sh 'mvn clean package'
 }
 
-def call() {
+def checkoutcode() {
     echo 'Checking out code...'
     checkout scm
 }
 
-def call() {
+def cleanup() {
     echo 'Cleaning up...'
     sh 'pkill -f "mvn spring-boot:run" || true'
 }
 
-def call() {
+def cleanupworkspace() {
     echo 'Cleaning up the workspace...'
     deleteDir() // Deletes all files in the current workspace
 }
 
-def call() {
+def runapplication() {
     echo 'Running Spring Boot application...'
     sh 'nohup mvn spring-boot:run &'
     sleep(time: 15, unit: 'SECONDS')
@@ -27,23 +27,23 @@ def call() {
     echo "The application is running and accessible at: http://${publicIp}:8080"
 }
 
-def call() {
+def setupjava() {
     echo 'Setting up Java 17...'
     sh 'sudo apt update'
     sh 'sudo apt install -y openjdk-17-jdk'
 }
 
-def call() {
+def setupmvn() {
     echo 'Setting up Maven...'
     sh 'sudo apt install -y maven'
 }
 
-def call() {
+def stopmvn() {
     echo 'Gracefully stopping the Spring Boot application...'
     sh 'mvn spring-boot:stop'
 }
 
-def call(String tagName, String message = 'Build tagging') {
+def tagbuild(String tagName, String message = 'Build tagging') {
     echo "Tagging the build with tag: ${tagName}"
     
     // Tagging the current commit in Git
@@ -53,12 +53,12 @@ def call(String tagName, String message = 'Build tagging') {
     """
 }
 
-def call(String artifactPath) {
+def aploadArtifcat(String artifactPath) {
     echo 'Uploading artifact...'
     archiveArtifacts artifacts: artifactPath, allowEmptyArchive: true
 }
 
-def call() {
+def validateapp() {
     echo 'Validating that the app is running...'
     def response = sh(script: 'curl --write-out "%{http_code}" --silent --output /dev/null http://localhost:8080', returnStdout: true).trim()
     if (response == "200") {
